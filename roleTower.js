cce833ac74754a0dcab1e9b0e3bb540b33b82cb0
MatthewFloyd <floydmatt22@gmail.com>
2017-12-25 16:21:12 -0700

    Updated target selection slightly
    (still needs some dedicated work)

--- roleTower.js
+++ roleTower.js
@@ -13,8 +13,14 @@ var roleTower = {
         var hostiles = tower.room.find(FIND_HOSTILE_CREEPS);
         if(hostiles.length > 0)
         {
-            let nearest = tower.pos.findClosestByRange(hostiles);
-            tower.attack(nearest);
+            let nearest = hostiles.filter(function(creep) {
+                return ((tower.pos.x - creep.pos.x) + (tower.pos.y - creep.pos.y) <= 20)
+            });
+            //console.log(nearest[0].pos.x - tower.pos.x + " " + nearest[0].pos.y - tower.pos.y);
+            if(nearest.length > 0)
+            {
+                tower.attack(nearest[0]);
+            }
         }
         if(tower.hits < (tower.hitsMax / 4))
         {
