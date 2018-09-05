898d183176f8d044d8f239bff24bbffce076bc84
MatthewFloyd <floydmatt22@gmail.com>
2018-09-05 16:55:26 -0600

    Update genericCreep.js

--- genericCreep.js
+++ genericCreep.js
@@ -3,7 +3,43 @@
 var genericCreep = {
     run: function(creep)
     {
-        
+        const total = _.sum(creep.carry);
+        if(total < creep.carryCapacity) // we don't have full energy so decide if we are at source or not
+        {
+            // get source from creep memory
+            var source = creep.memory.sourceId;
+            var source = Game.getObjectById(source);
+            if(creep.pos.isNearTo(source)) // at the source
+            {
+                if(creep.harvest(source) !== 0)
+                {
+                    console.log(creep.memory.role + " has an error harvesting source!");
+                }
+            }
+            else // not at source
+            {
+                
+            }
+        }
+        else // we have full energy
+        {
+            // set new destination and start travel
+            
+            // check spawn to see if energy needed, otherwise upgrade controller
+            var spawn = creep.room.memory.spawns[0]; // update later
+            var spawn = Game.getObjectById(spawn);
+            // check for case at spawn and full energy
+            if(creep.pos.isNearTo(spawn)) // at spawn
+            {
+                // TODO
+            }
+            if(spawn.energy < (spawn.energyCapacity - creep.carryCapacity)) // is it worth it?
+            {
+                const newTravelDest = spawn.pos.x + " " + spawn.pos.y + creep.room.name;
+                creep.memory.traveDest = newTravelDest;
+                creep.memory.travel = true;
+            }
+        }
     }
 };
 
